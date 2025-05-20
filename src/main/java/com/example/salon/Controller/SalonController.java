cd77abd590b2a55bfb7b6f829b673ee0e07864dc
adeshpotphode <adeshpotphoe@gmail.com>
2025-05-20 20:20:18 +0530

    6 commit

--- src/main/java/com/example/salon/Controller/SalonController.java
+++ src/main/java/com/example/salon/Controller/SalonController.java
@@ -20,7 +20,7 @@ public class SalonController {
     @GetMapping("/")
     public String index()
     {
-        return"/1stbean/index";
+        return"/index";
     }
 
     @GetMapping("/AdminPage")
